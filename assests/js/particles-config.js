f4d01c685f7fc1093b1f089e42df8e6411de1655
AKSHAYA SRINITHI SV <akshayasri2908@gmail.com>
2024-10-15 17:19:23 +0530

    Updated portfolio home page with new AI/ML design

---
+++ assests/js/particles-config.js
@@ -0,0 +1,45 @@
+particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 80
+      },
+      "color": {
+        "value": "#00ff88"
+      },
+      "shape": {
+        "type": "circle"
+      },
+      "opacity": {
+        "value": 0.5
+      },
+      "size": {
+        "value": 3
+      },
+      "move": {
+        "speed": 6
+      }
+    }
+  });
+  particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 80
+      },
+      "color": {
+        "value": "#00ff88"
+      },
+      "shape": {
+        "type": "circle"
+      },
+      "opacity": {
+        "value": 0.5
+      },
+      "size": {
+        "value": 3
+      },
+      "move": {
+        "speed": 6
+      }
+    }
+  });
+    
