fb7583b58f0f3ba14eafc25162a932fd3f42391d
AKSHAYA SRINITHI SV <akshayasri2908@gmail.com>
2024-10-15 17:45:54 +0530

    Updated homepage with animated particles

--- assests/js/particles-config.js
+++ assests/js/particles-config.js
@@ -1,45 +1,24 @@
 particlesJS("particles-js", {
-    "particles": {
-      "number": {
-        "value": 80
-      },
-      "color": {
-        "value": "#00ff88"
-      },
-      "shape": {
-        "type": "circle"
-      },
-      "opacity": {
-        "value": 0.5
-      },
-      "size": {
-        "value": 3
-      },
-      "move": {
-        "speed": 6
-      }
+  particles: {
+    number: { value: 100, density: { enable: true, value_area: 800 } },
+    color: { value: ["#36C5F0", "#2EB67D", "#ECB22E", "#E01E5B"] },
+    shape: { type: "circle", stroke: { width: 0, color: "#fff" } },
+    opacity: { value: 1 },
+    size: { value: 6, random: true },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#808080",
+      opacity: 0.4,
+      width: 1
+    },
+    move: { enable: true, speed: 4, out_mode: "out" }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "repulse" }
     }
-  });
-  particlesJS("particles-js", {
-    "particles": {
-      "number": {
-        "value": 80
-      },
-      "color": {
-        "value": "#00ff88"
-      },
-      "shape": {
-        "type": "circle"
-      },
-      "opacity": {
-        "value": 0.5
-      },
-      "size": {
-        "value": 3
-      },
-      "move": {
-        "speed": 6
-      }
-    }
-  });
-    
+  },
+  retina_detect: true
+});
